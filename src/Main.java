58a5a633df52e9624ad769584b3d6b8b268a2f40
student <samoilovid951@gmail.com>
2019-10-05 21:49:49 +0300

    fixes #3, #4

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+
+public class Main {
+    public static void main(String[] args) {
+        int totalValueOfAllPreviousPurchases= 100000;
+        int currentPurchasePrice= 5000;
+        int fixedAmount = 1000;
+        int bonusStandardLevel=50;
+        int bonusSilverLevel=70;
+        int bonusGoldLevel =100;
+        int priceLimitOne = 15_000;
+        int priceLimitTwo = 150_000;
+        if (totalValueOfAllPreviousPurchases<= priceLimitOne) {
+        int accruedBonuses = currentPurchasePrice/fixedAmount*bonusStandardLevel;
+        System.out.println(accruedBonuses);
+        }
+        else {
+
+        if (totalValueOfAllPreviousPurchases<= priceLimitTwo){
+        int accruedBonuses = currentPurchasePrice/fixedAmount*bonusSilverLevel;
+        System.out.println(accruedBonuses);
+        }
+        else if (totalValueOfAllPreviousPurchases> priceLimitTwo){
+        int accruedBonuses = currentPurchasePrice/fixedAmount*bonusSilverLevel;
+        System.out.println(accruedBonuses);
+        }
+        }
+    }
+
+}
